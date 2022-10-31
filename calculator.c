daa9b354c018e8c9d98a688ae30f8149f4b37964
shuvo794 <shuvohossain794@gmail.com>
2022-10-31 17:14:58 +0600

    calculator work

--- calculator.c
+++ calculator.c
@@ -7,13 +7,18 @@ int main(){
     float num1,num2,result;
     scanf("%f  %f",&num1,&num2); 
     result=num1+num2;
-    printf("%2.f + %2.f=%f\n",num1,num2,result);
+
+    printf("%.2f + %.2f=%.2f\n",num1,num2,result);
+    
     result=num1-num2;
-    printf("%2.f - %2.f=%f\n",num1,num2,result);
+    
+    printf("%.2f - %.2f=%.2f\n",num1,num2,result);
+    
     result=num1*num2;
-    printf("%2.f*%2.f\n",num1,num2,result);
+    printf("%.2f *  %.2f=%.2f\n",num1,num2,result);
+    
     result =num1/num2;
-    printf("%2.f / %2.f\n",num1,num2,result);
+    printf("%.2f / %.2f=%.2f\n",num1,num2,result);
 
 
 
