f19e3ed25df8c040a6e22ee1e84d9df7fe94b9e4
shuvo794 <shuvohossain794@gmail.com>
2022-10-31 16:56:53 +0600

    sand code

--- calculator.c
+++ calculator.c
@@ -5,11 +5,9 @@ int main(){
     printf("Please give a input digit , ex (3,5)");
 
     float num1,num2,result;
- 
+    scanf("%f  %f",&num1,&num2); 
     result=num1+num2;
-    
-    scanf("%f  %f",&num1,num2); 
-
+     
 
     return 0;
 }
