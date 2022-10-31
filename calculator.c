af8de591a2e66123af9d1fa10f05ca2403b23dcc
shuvo794 <shuvohossain794@gmail.com>
2022-10-31 17:17:04 +0600

    calculator work

--- calculator.c
+++ calculator.c
@@ -5,7 +5,9 @@ int main(){
     printf("Please give a input digit , ex (3,5)");
 
     float num1,num2,result;
+
     scanf("%f  %f",&num1,&num2); 
+    
     result=num1+num2;
 
     printf("%.2f + %.2f=%.2f\n",num1,num2,result);
@@ -15,9 +17,11 @@ int main(){
     printf("%.2f - %.2f=%.2f\n",num1,num2,result);
     
     result=num1*num2;
+    
     printf("%.2f *  %.2f=%.2f\n",num1,num2,result);
     
     result =num1/num2;
+    
     printf("%.2f / %.2f=%.2f\n",num1,num2,result);
 
 
