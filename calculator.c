da521fd59a1199b5b01b3916326ee3c9fc46cc44
shuvo794 <shuvohossain794@gmail.com>
2022-10-31 17:02:13 +0600

    sand code

--- calculator.c
+++ calculator.c
@@ -7,7 +7,11 @@ int main(){
     float num1,num2,result;
     scanf("%f  %f",&num1,&num2); 
     result=num1+num2;
-     
+    printf("%2.f + %2.f=%f\n",num1,num2,result);
+    result=num1-num2;
+    printf("%2.f - %2.f=%f\n",num1,num2,result);
+    
+
 
     return 0;
 }
